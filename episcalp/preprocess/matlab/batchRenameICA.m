948a9493de17c9e554673e8475123a82a0cdb2ed
Patrick Myers <pmyers16@jhu.edu>
2021-03-23 12:47:26 -0400

    add bids conversion script

--- episcalp/preprocess/matlab/batchRenameICA.m
+++ episcalp/preprocess/matlab/batchRenameICA.m
@@ -1,9 +1,15 @@
-sourcedir = 'D:\ScalpData\R01_ministudy\ICA_data';
-fileList = dir(fullfile(sourcedir, '*.set'));
+EEGLabPath = "D:/Downloads/eeglab_default/eeglab2020_0";
+%EEGLabPath = '/home/adam2392/Documents/eeglab2021.0'; % change this
+addpath(genpath(EEGLabPath))
+
+
+sourcedir = 'D:/ScalpData/test_convert/derivatives/ICA/sourcedata/normal';
+fileList = dir(fullfile(sourcedir, '**/*.set'));
 for find = 1:length(fileList)
     fname = fileList(find).name;
+    fdir = fileList(find).folder;
     disp(fname)
-    EEG = pop_loadset('filename', fname, 'filepath', sourcedir);
+    EEG = pop_loadset('filename', fname, 'filepath', fdir);
     EEG = eeg_checkset(EEG);
-    EEG = pop_saveset(EEG, 'filename', fname, 'filepath', sourcedir);
+    EEG = pop_saveset(EEG, 'filename', fname, 'filepath', fdir);
 end
