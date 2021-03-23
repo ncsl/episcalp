2f7bc2a6080551ee1872e68cdffd55fad30aa395
Patrick Myers <pmyers16@jhu.edu>
2021-03-23 13:21:49 -0400

    add instructions for batchRename

--- episcalp/preprocess/matlab/batchRenameICA.m
+++ episcalp/preprocess/matlab/batchRenameICA.m
@@ -1,3 +1,10 @@
+%%
+% 1. Move all the .set and .fdt files to a new folder
+% 2. Change the sourcedir path below to the new folder path
+% The .set file needs to point to the .fdt file, so this will modify
+% the internal pointer
+
+%%
 EEGLabPath = "D:/Downloads/eeglab_default/eeglab2020_0";
 %EEGLabPath = '/home/adam2392/Documents/eeglab2021.0'; % change this
 addpath(genpath(EEGLabPath))
