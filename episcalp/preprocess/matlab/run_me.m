fa736a41634d5d4b3835ff7c62fd111d733d370a
Adam Li <adam2392@gmail.com>
2021-03-23 17:42:33 -0400

    Pushing chnages for fixing sidecar files.

--- episcalp/preprocess/matlab/run_me.m
+++ episcalp/preprocess/matlab/run_me.m
@@ -2,23 +2,33 @@ EEGLabPath = '/home/adam2392/Documents/eeglab2021.0'; % change this
 addpath(genpath(EEGLabPath))
 
 %folder = "D:/ScalpData/test_parallel/sourcedata/normal";  % change this
-folder = '/home/adam2392/hdd3/tuh_normal_vs_abnormal/sourcedata/normal';
-dataset = 'tuh_normal_vs_abnormal';  % used to save the temporary files, not important
+folder = '/home/adam2392/hdd3/tuh_epilepsy_vs_normal/sourcedata/no_epilepsy';
+folder = '/home/adam2392/hdd3/tuh_epileptic_abnormal_vs_normal_EEG/sourcedata/abnormal/';
+dataset = 'tuh_epilepsy_vs_normal';  % used to save the temporary files, not important
 extension = '.edf';
 %outputdir = "D:/ScalpData/test_parallel/outdir";  % change this
-outputdir = '/home/adam2392/hdd3/tuh_normal_vs_abnormal/derivatives/ICA/normal';
+outputdir = '/home/adam2392/hdd3/tuh_epilepsy_vs_normal/derivatives/ICA/no_epilepsy';
+outputdir = '/home/adam2392/hdd3/tuh_epileptic_abnormal_vs_normal_EEG/derivatives/ICA/abnormal/';
 
 group_size=5; % chunk the data to make computation a little faster
-path_separator = "/";  % change for Windows/Linux systems \ or /
+path_separator = '/';  % change for Windows/Linux systems \ or /
 
 % Grab just the names of just the edf files
 files = dir(folder);
 ext_files = {};
 for find = 1:length(files)
     fname = files(find).name;
-    fname = folder + path_separator + fname;
-    if (contains(fname, extension))
-        ext_files{end+1} = fname;
+    % check if we have already run the file
+    expected_name = strrep(fname, extension, '.set');
+    expected_path = fullfile(outputdir, expected_name);
+    if ~isfile(expected_path)
+        %fname = folder + path_separator + fname;
+        fname = fullfile(folder, fname);
+        %disp(strcat("About to run this file: ", fname))
+        %disp(expected_path);
+        if (contains(fname, extension))
+            ext_files{end+1} = fname;
+        end
     end
 end
 count = 1;
@@ -30,6 +40,7 @@ for find=1:group_size:length(ext_files)
         stop_ind = find+group_size-1;
     end
     files = {ext_files{find:stop_ind}};
+    disp(files)
     % Perform freq-based filtering on the raw edf files
     temp_mat_fpath = batchFilter(folder, dataset, files, count);
     % load in the structure
--- examples/scripts/converting_ica_to_bids.py
+++ examples/scripts/converting_ica_to_bids.py
@@ -1,11 +1,14 @@
 from pathlib import Path
-from mne_bids import BIDSPath
+from mne_bids import BIDSPath, update_sidecar_json, read_raw_bids
 
 from episcalp.bids.bids_conversion import write_epitrack_bids, append_original_fname_to_scans
 
 
-def convert_ica_to_bids(root, bids_identifiers, montage="standard_1020", ext=".set", to_ext=".edf"):
-    source_dir = root / "sourcedata"
+def convert_ica_to_bids(root, source_dir, bids_identifiers,
+                        montage="standard_1020", ext=".set", to_ext=".vhdr"):
+    datatype = 'eeg'
+    suffix = 'eeg'
+
     # Find all the potential files in the sourcedata directory
     source_fpaths = [
         fpath
@@ -20,9 +23,13 @@ def convert_ica_to_bids(root, bids_identifiers, montage="standard_1020", ext=".s
         session = session.replace("s", "")
         run = run.replace(ext, "").replace("t", "")
         task = fpath.parent.name
-        bids_identifiers.update(subject=subject, session=session, task=task, run=run, root=root, extension=to_ext)
+        bids_identifiers.update(subject=subject, session=session, task=task,
+                                run=run, root=root, extension=to_ext,
+                                datatype=datatype, suffix=datatype)
         bids_path = BIDSPath(**bids_identifiers)
 
+        bids_path.directory.mkdir(exist_ok=True, parents=True)
+        print(f'Converting {bids_path} to BIDS... of {fpath}')
         bids_path = write_epitrack_bids(
             source_path=fpath,
             bids_path=bids_path,
@@ -36,10 +43,51 @@ def convert_ica_to_bids(root, bids_identifiers, montage="standard_1020", ext=".s
         # back-tracking
         append_original_fname_to_scans(Path(fpath).name, root, bids_path.basename)
 
+        # update sidecar json
+        sidecar_path = bids_path.copy().update(extension='.json')
+        raw = read_raw_bids(bids_path)
+        sfreq = raw.info['sfreq']
+        line_freq = raw.info['line_freq']
+        freqs = ''  # function of line frequency and sampling freq
+
+        # ICA pipeline filtered apriori
+        # update_dict = {
+        #     'SoftwareFilters': {
+        #         'notch filter': {
+        #             'order': '',
+        #             'freqs': freqs,
+        #         },
+        #         'band-pass filter': {
+        #             'l_freq': '',
+        #             'h_freq': '',
+        #         }
+        #     }
+        # }
+        # update_sidecar_json(sidecar_path, update_dict)
+
+        # update description.json
+        descrip_path = BIDSPath(root=bids_path.root, suffix='dataset_description',
+                                extension='.json', check=False)
+        update_dict = {
+            'DatasetType': 'derivative',
+            'Name': 'EEGLab binICA',
+            'Version': '',  # include version used
+            'Description': 'See README for more info. Automated ICA procedure was ran with binICA from '
+                           'EEGLab and then saved as .set files. Preprocessing, such as basic filtering '
+                           'was done. Detected ICA components were removed from the data as part of '
+                           'the automated procedure.'
+        }
+        update_sidecar_json(descrip_path, update_dict)
+        break
+
 
 if __name__ == "__main__":
-    bids_root = Path("D:/ScalpData/test_convert/derivatives/ICA")
+    root = Path("D:/ScalpData/test_convert/derivatives/ICA")
+    root = Path("/home/adam2392/hdd3/tuh_epileptic_abnormal_vs_normal_EEG/derivatives/ICA")
+    root = Path("/home/adam2392/hdd3/tuh_epilepsy_vs_normal/derivatives/ICA")
+
+    source_dir = root / "sourcedata"
     bids_identifiers = {
-        "acquisition": "eeg"
+        # "acquisition": "eeg"
     }
-    convert_ica_to_bids(bids_root, bids_identifiers)
+    convert_ica_to_bids(root, source_dir, bids_identifiers)
