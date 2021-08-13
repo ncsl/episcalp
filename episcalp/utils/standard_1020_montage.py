547dff68f464a7d86efe37b56e4641f5daaab474
Patrick Myers <pmyers16@jhu.edu>
2021-08-13 12:50:36 -0400

    add spikes to events script

---
+++ episcalp/utils/standard_1020_montage.py
@@ -0,0 +1,4 @@
+
+def get_standard_1020_montage():
+    return ["Fp1", "Fp2", "F3", "F4", "F7", "F8", "P3", "P4", "C3", "C4", "P7", "P8", "O1", "O2", "T3", "T4", "T5",
+            "T6", "T7", "T8", "Cz", "Pz", "Fz"]
