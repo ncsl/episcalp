0ce183dc11726db3639ad02d29c753ab8d81e02f
Adam Li <adam2392@gmail.com>
2021-09-16 14:46:19 -0400

    Robust spike and bids conversion of ICA scripts

--- episcalp/scripts/bids/run_bids_conversion_ica.py
+++ episcalp/scripts/bids/run_bids_conversion_ica.py
@@ -2,9 +2,15 @@ from pathlib import Path
 import shutil as sh
 import os
 from tempfile import TemporaryDirectory, tempdir
-from mne_bids import BIDSPath, make_dataset_description, update_sidecar_json
+from mne_bids import (
+    BIDSPath,
+    make_dataset_description,
+    update_sidecar_json,
+    read_raw_bids,
+)
 from mne_bids.path import get_entities_from_fname, get_entity_vals, print_dir_tree
 from natsort import natsorted
+from episcalp import bids
 
 from episcalp.bids import write_epitrack_bids, append_original_fname_to_scans
 
@@ -12,16 +18,17 @@ from episcalp.bids import write_epitrack_bids, append_original_fname_to_scans
 def convert_eeglab_ica_to_bids():
     root = Path("D:/OneDriveParent/OneDrive - Johns Hopkins/Shared Documents/40Hz-30")
     root = Path("/Users/adam2392/Johns Hopkins/Scalp EEG JHH - Documents/bids/")
+    root = Path("/Users/adam2392/Johns Hopkins/Jefferson_Scalp - Documents/root/")
 
     filter_scheme = "1-30Hz-30"
     window_scheme = "win-20"
     authors = "Patrick Myers and Adam Li"
-    dataset_name = "JHH-ICLabel-Cleaned"
+    dataset_name = "Jefferson-ICLabel-Cleaned"
 
     # root and filtering scheme
     ica_root = root / "derivatives" / "ICA" / filter_scheme / window_scheme
     ica_src = ica_root / "sourcedata"
-    ext = ".set"
+    ica_ext = ".set"
 
     # BIDS entities
     montage = "standard_1020"
@@ -32,24 +39,26 @@ def convert_eeglab_ica_to_bids():
     extension = ".edf"
 
     # get all subjects
-    # subjects = natsorted(list(ica_src.glob('*')))
     subjects = get_entity_vals(ica_src, "subject")
+
     print("Got these subjects...", subjects)
     for subject in subjects:
         subj_src = ica_src / f"sub-{subject}"
         # get all subject file paths
-        subject_fpaths = list(subj_src.glob(f"*{subject}*{ext}"))
+        subject_fpaths = natsorted(list(subj_src.glob(f"*{subject}*{ica_ext}")))
 
-        for fpath in subject_fpaths:
+        for idx, fpath in enumerate(subject_fpaths):
             entities = get_entities_from_fname(fpath.name)
-            print(entities)
+            print("Entities: ", entities)
             print(fpath)
             bids_path = BIDSPath(
                 root=ica_root,
                 datatype=datatype,
+                extension=extension,
                 **entities
                 #  subject=subject, suffix=suffix
             )
+            run = bids_path.run
 
             # write to BIDS again
             bids_path = write_epitrack_bids(
@@ -86,25 +95,57 @@ def convert_eeglab_ica_to_bids():
             part_dest = ica_root / "participants.tsv"
             sh.copyfile(part_src, part_dest)
 
-            # copy files for each subject
-            # subject_files =  list(BIDSPath(subject=subject, root=root).fpath.rglob('*'))
-            subj_root = BIDSPath(subject=subject, root=root).directory
-            with TemporaryDirectory() as fdir:
-                temp_root = Path(fdir) / f"sub-{subject}"
-                sh.copytree(subj_root, temp_root)
-                # print_dir_tree(fdir)
-                subject_files = BIDSPath(
-                    subject=subject, root=fdir, extension=extension
-                ).match()
-                for fpath in subject_files:
-                    os.remove(fpath)
-                # print_dir_tree(fdir)
-
             # append scans original filenames
             append_original_fname_to_scans(
                 Path(fpath).name, ica_root, bids_path.basename
             )
 
 
+def _merge_src_metadata():
+    root = Path("D:/OneDriveParent/OneDrive - Johns Hopkins/Shared Documents/40Hz-30")
+    root = Path("/Users/adam2392/Johns Hopkins/Scalp EEG JHH - Documents/bids/")
+    root = Path("/Users/adam2392/Johns Hopkins/Jefferson_Scalp - Documents/root/")
+
+    filter_scheme = "1-30Hz-30"
+    window_scheme = "win-20"
+
+    # root and filtering scheme
+    ica_root = root / "derivatives" / "ICA" / filter_scheme / window_scheme
+
+    # BIDS entities
+    datatype = "eeg"
+    suffix = "eeg"
+
+    # get all subjects
+    subjects = get_entity_vals(ica_root, "subject")
+    src_subjects = get_entity_vals(root, "subject")
+    for subject in subjects:
+        # find the corresponding raw source subjects
+        if subject not in src_subjects:
+            raise RuntimeError(f"{subject} not in the original raw source {root}.")
+
+        # get all metadata files
+        suffixes = ["events"]
+        for suffix in suffixes:
+            bids_path = BIDSPath(
+                subject=subject, datatype=datatype, suffix=suffix, root=root
+            )
+
+            matched_files = bids_path.match()
+            for meta_fpath in matched_files:
+                dest_fpath = BIDSPath(
+                    root=ica_root,
+                    datatype=meta_fpath.datatype,
+                    extension=meta_fpath.extension,
+                    suffix=meta_fpath.suffix,
+                    **meta_fpath.entities,
+                )
+
+                # copy over the file
+                print(f"Copied {meta_fpath} to {dest_fpath}")
+                sh.copyfile(meta_fpath.fpath, dest_fpath.fpath)
+
+
 if __name__ == "__main__":
-    convert_eeglab_ica_to_bids()
+    # convert_eeglab_ica_to_bids()
+    _merge_src_metadata()
