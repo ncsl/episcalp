cc23870d8f3ae249b4dc279aa9cc2e5d61cc9114
Patrick Myers <pmyers16@jhu.edu>
2021-10-26 15:41:11 -0400

    parametrize winsize and stepsize

--- episcalp/scripts/sourcesink/run_sourcesink_analysis.py
+++ episcalp/scripts/sourcesink/run_sourcesink_analysis.py
@@ -41,8 +41,10 @@ def run_sourcesink_analysis(
     root = bids_path.root
     rereference = False
 
+    winsize = model_params["winsize"]
+    stepsize = model_params["stepsize"]
     # get the root derivative path
-    deriv_chain = Path("sourcesink") / reference / f"sub-{subject}"
+    deriv_chain = Path("sourcesink") / f"win-{winsize}" / f"step-{stepsize}" / reference / f"sub-{subject}"
     figures_path = figures_path / "figures" / deriv_chain
     raw_figures_path = root / "derivatives" / "figures" / "raw" / f"sub-{subject}"
     deriv_path = deriv_path / deriv_chain
@@ -72,12 +74,8 @@ def run_sourcesink_analysis(
     # use the same basename to save the data
     raw.drop_channels(raw.info["bads"])
 
-    model_params = {
-        "winsize": 500,
-        "stepsize": 250,
-    }
     # run heatmap
-    ss_deriv, sink_deriv, state_arr_deriv = lds_raw_sourcesink(
+    ss_deriv, sink_deriv, source_infl_deriv, sink_conn_deriv, state_arr_deriv = lds_raw_sourcesink(
         raw,
         reference=reference,
         return_all=True,
@@ -86,9 +84,15 @@ def run_sourcesink_analysis(
     )
 
     ss_deriv_fpath = deriv_path / ss_deriv.info._expected_basename
+    sink_deriv_fpath = deriv_path / sink_deriv.info._expected_basename
+    source_infl_deriv_fpath = deriv_path / source_infl_deriv.info._expected_basename
+    sink_conn_deriv_fpath = deriv_path / sink_conn_deriv.info._expected_basename
     state_deriv_fpath = deriv_path / state_arr_deriv.info._expected_basename
 
     ss_deriv.save(ss_deriv_fpath, overwrite=overwrite)
+    sink_deriv.save(sink_deriv_fpath, overwrite=overwrite)
+    source_infl_deriv.save(source_infl_deriv_fpath, overwrite=overwrite)
+    sink_conn_deriv.save(sink_conn_deriv_fpath, overwrite=overwrite)
     state_arr_deriv.save(state_deriv_fpath, overwrite=overwrite)
 
     # normalize and plot heatmap
@@ -126,6 +130,51 @@ def run_sourcesink_analysis(
             figure_fpath=(figures_path / fig_basename),
         )
 
+        fig_basename = fig_basename.replace("ssind", "sinkind")
+
+        sink_deriv.plot_heatmap(
+            soz_chs=resected_chs,
+            cbarlabel="Fragility",
+            cmap="turbo",
+            # vertical_markers=vertical_markers,
+            # soz_chs=soz_chs,
+            # figsize=(10, 8),
+            # fontsize=12,
+            # vmax=0.8,
+            title=fig_basename,
+            figure_fpath=(figures_path / fig_basename),
+        )
+
+        fig_basename = fig_basename.replace("sinkind", "sourceinflind")
+
+        source_infl_deriv.plot_heatmap(
+            soz_chs=resected_chs,
+            cbarlabel="Fragility",
+            cmap="turbo",
+            # vertical_markers=vertical_markers,
+            # soz_chs=soz_chs,
+            # figsize=(10, 8),
+            # fontsize=12,
+            # vmax=0.8,
+            title=fig_basename,
+            figure_fpath=(figures_path / fig_basename),
+        )
+
+        fig_basename = fig_basename.replace("sourceinflind", "sinkconnind")
+
+        sink_conn_deriv.plot_heatmap(
+            soz_chs=resected_chs,
+            cbarlabel="Fragility",
+            cmap="turbo",
+            # vertical_markers=vertical_markers,
+            # soz_chs=soz_chs,
+            # figsize=(10, 8),
+            # fontsize=12,
+            # vmax=0.8,
+            title=fig_basename,
+            figure_fpath=(figures_path / fig_basename),
+        )
+
         fig_basename = fig_basename.replace(figure_ext, f"_topomap{figure_ext}")
         ss_deriv.plot_topomap(
             # soz_chs=resected_chs,
@@ -151,6 +200,12 @@ def run_post_analysis(deriv_path=None, subject=None, features=None):
 
 
 if __name__ == "__main__":
+
+    model_params = {
+        "winsize": 100,
+        "stepsize": 50,
+    }
+
     root = Path(
         "D:/OneDriveParent/Johns Hopkins/Jefferson_Scalp - Documents/root/derivatives/ICA/1-30Hz-30/win-20"
     )
@@ -202,4 +257,5 @@ if __name__ == "__main__":
                         resample_sfreq=256,
                         overwrite=True,
                         figure_ext=figure_ext,
+                        **model_params
                     )
