cdfc1f8290f0ef33a173613a76d14d006ca9566a
Patrick Myers <pmyers16@jhu.edu>
2021-12-14 16:09:12 -0500

    enable pca analysis

--- episcalp/features.py
+++ episcalp/features.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy.testing import assert_array_equal
 from scipy.stats import entropy, skew, kurtosis
+from sklearn.decomposition import PCA
 
 from .montage import _standard_lobes
 
@@ -69,15 +70,18 @@ def spike_feature_vector(ch_spike_df, ch_names, type="rate"):
 
 def heatmap_features(feature_map, ch_names=None, types=None):
     if types is None:
-        types = ["quantile"]
+        types = {"quantile": None}
+    if type(types) == list:
+        type_names = types
+    else:
+        type_names = [fn for fn in types.keys()]
     if ch_names is None and "lobes" in types:
         raise ValueError('ch_names must be passed to calculate lobe features')
     # average over time
     this_data = np.nanmean(feature_map, axis=1)
-    # features = np.empty((0,))
 
     feature_vec = []
-    if "quantile" in types:
+    if "quantile" in type_names:
         # distributional features of the EEG electrodes
         quantile_vec = np.hstack(
             [np.quantile(this_data, q=q) for q in [0.1, 0.5, 0.9]]
@@ -85,23 +89,24 @@ def heatmap_features(feature_map, ch_names=None, types=None):
             + [this_data.std()]
         )
         feature_vec.extend(list(quantile_vec))
-    if "spatial" in types:
+    if "spatial" in type_names:
         spatial_vec = this_data
         feature_vec.extend(spatial_vec)
 
-    if "lobes" in types:
+    if "lobes" in type_names:
         lobe_dict = _standard_lobes(separate_hemispheres=False)
         lobe_vals = []
         for lobe, lobe_chs in lobe_dict.items():
             idx = [idx for idx in range(len(ch_names)) if ch_names[idx] in lobe_chs]
             if idx == []:
-                #lobe_vals.append(-1)
+                lobe_vals.append(-1)
+                lobe_vals.append(-1)
                 continue
             lobe_vals.append(np.nanmean(this_data[idx]))
             lobe_vals.append(np.std(this_data[idx]))
         feature_vec.extend(lobe_vals)
 
-    if "distribution" in types:
+    if "distribution" in type_names:
         n_chs = len(ch_names)
         distribution_vals = []
         uni_dist = np.ones((n_chs, 1)) / n_chs
@@ -111,11 +116,54 @@ def heatmap_features(feature_map, ch_names=None, types=None):
         distribution_vals.append(np.var(this_data))  # variance
         distribution_vals.append(skew(this_data))  # skew
         distribution_vals.append(kurtosis(this_data))  # kurtosis
-        # distribution_vals.append(np.mean(this_data))
 
         distribution_vals.append(entropy(this_data, uni_dist))  # kl divergence from uniform
 
         feature_vec.extend(distribution_vals)
 
-    #feature_vec = np.array(feature_vec)
+    if "pca-channels" in type_names:
+        pca_kwargs = types["pca-channels"]
+        n_comp = pca_kwargs.get("n_components", 1)
+        n_chan = feature_map.shape[0]
+        pca = PCA(n_components=n_comp)
+        pca.fit(feature_map)
+
+        transformed_data = []
+        for idx in range(n_chan):
+            pcai = pca.transform(feature_map[idx, :].reshape(1, -1))
+            transformed_data.append(pcai[0][0])
+        feature_vec.extend(transformed_data)
+
+    if "pca-lobes" in type_names:
+        pca_kwargs = types["pca-lobes"]
+        n_comp = pca_kwargs.get("n_components", 1)
+        pca = PCA(n_components=n_comp)
+        pca.fit(feature_map)
+
+        separate = pca_kwargs.get("separate", False)
+        lobe_dict = _standard_lobes(separate_hemispheres=separate)
+
+        return_vals = pca_kwargs.get("return_vals", ["mean", "std"])
+        lobe_vals = []
+        for lobe, lobe_chs in lobe_dict.items():
+            idx = [idx for idx in range(len(ch_names)) if ch_names[idx] in lobe_chs]
+            if idx == []:
+                lobe_vals.append(-1)
+                lobe_vals.append(-1)
+                continue
+
+            # Calculate PCA of feature for this lobe
+            lobe_mat = feature_map[idx, :]
+            lobe_vec = []
+            for cdx in range(lobe_mat.shape[0]):
+                pcai = pca.transform(lobe_mat[cdx, :].reshape(1, -1))
+                lobe_vec.append(pcai[0][0])
+
+            # Add mean and standard deviation of pca vals as features
+            if "mean" in return_vals:
+                lobe_vals.append(np.nanmean(lobe_vec))
+            if "std" in return_vals:
+                lobe_vals.append(np.std(lobe_vec))
+        feature_vec.extend(lobe_vals)
+
     return feature_vec
