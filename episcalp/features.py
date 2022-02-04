b96ce7c4aa46e6a24e71758d77b5e5e5fe578528
Kristin Maria Gunnarsdottir <kgunnar1@jhu.edu>
2022-02-04 09:51:05 +0000

    Update features.py
    adding heatmap_features2 which computes features for each timepoint before applying summary method

--- episcalp/features.py
+++ episcalp/features.py
@@ -141,3 +141,80 @@ def heatmap_features(feature_map, types, ch_names=None, summary_method="average"
             feature_vec.extend(distribution_vals)
 
     return feature_vec
+
+def heatmap_features2(feature_map, types, ch_names=None, summary_method="average", **kwargs):
+    if types is None:
+        return None
+    elif len(types) == 0:
+        return None
+    type_names = types
+    if ch_names is None and "lobes" in types:
+        raise ValueError('ch_names must be passed to calculate lobe features')
+        
+    n_chan = feature_map.shape[0]
+    n_timepoints = feature_map.shape[1]
+    
+    # Compute features before reducing dimensions 
+    if "quantile" in type_names:
+        big_feature_vec = np.zeros((5,n_timepoints))
+        # Compute features before reducing dimensions 
+        for col in range(n_timepoints):
+            feature_map_ = np.array([td[col] for td in feature_map])
+
+            # quantile features of the EEG electrodes
+            quantile_vec = np.hstack(
+                [np.quantile(feature_map_, q=q) for q in [0.1, 0.5, 0.9]]
+                + [feature_map_.mean()]
+                + [feature_map_.std()]
+            )
+            big_feature_vec[:,col] = quantile_vec
+
+    if "lobes" in type_names:
+        lobe_dict = _standard_lobes(separate_hemispheres=separate)
+        n_lobes = len(lobe_dict)
+
+        big_feature_vec = np.zeros((n_lobes*2,n_timepoints))
+        for col in range(n_timepoints):
+            feature_map_ = np.array([td[col] for td in feature_map])
+            i_lobe = -2
+            for lobe, lobe_chs in lobe_dict.items():
+                i_lobe+=2
+                idx = [idx for idx in range(len(ch_names)) if ch_names[idx] in lobe_chs]
+                if idx == []:
+                    big_feature_vec[i_lobe,:] = -1*np.ones((1,n_timepoints))
+                    big_feature_vec[i_lobe+1,:] = -1*np.ones((1,n_timepoints))
+                    continue
+                big_feature_vec[i_lobe,:]  = np.nanmean(feature_map[idx,:], axis=0)
+                big_feature_vec[i_lobe+1,:]  =np.std(feature_map[idx,:], axis=0)
+
+    # if "distribution" in type_names:
+        # n_chs = len(ch_names)
+        # distribution_vals = []
+        # uni_dist = np.ones((n_chs, 1)) / n_chs
+        # uni_dist = uni_dist.reshape((len(uni_dist),))
+
+        # for col in range(this_data.shape[1]):
+        #     this_data_ = np.array([td[col] for td in this_data])
+        #     distribution_vals.append(entropy(this_data_))  # entropy
+        #     distribution_vals.append(np.var(this_data_))  # variance
+        #     distribution_vals.append(skew(this_data_))  # skew
+        #     distribution_vals.append(kurtosis(this_data_))  # kurtosis
+
+        #     distribution_vals.append(entropy(this_data_, uni_dist))  # kl divergence from uniform
+
+        #     big_feature_vec.extend(distribution_vals)
+
+    # Reduce the dimensions of the data either by averaging or performing PCA on time domain
+    if summary_method == "average":
+        feature_vec_ = np.nanmean(big_feature_vec, axis=1).reshape(-1, 1)
+        feature_vec = [feature for avg_features in feature_vec_ for feature in avg_features]
+    elif summary_method == "pca":
+        n_components = kwargs.get("n_components", 1)
+        pca = PCA(n_components=n_components)
+        pca.fit(big_feature_vec)
+
+       feature_vec = pca.transform(big_feature_vec)        # Need to verify this is ok 
+       print(feature_vec)
+
+    return feature_vec
+
